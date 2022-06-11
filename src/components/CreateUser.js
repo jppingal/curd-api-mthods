5ed5ed01186216bfa62c88294a59d53bf40ea91f
Jai Prakash <jp.pingal1993@gmail.com>
2022-06-11 11:47:10 +0530

    Initial commit

---
+++ src/components/CreateUser.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CreateUser = () =>{
+    return(
+        <h1>CreateUser</h1>
+    )
+}
+export default CreateUser;
---
+++ src/components/UserTable.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserTable = () =>{
+    return(
+        <h1>UserTable</h1>
+    )
+}
+export default UserTable;
